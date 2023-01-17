379885ced745e666fce39d7020665f8cbaae69e7
attila <attila.istvan.toth@gmail.com>
2023-01-17 17:03:55 +0200

    Add Car class

---
+++ Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace GarageManager.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string ModelName { get; set; }
+        public string LicensePlate { get; set; }
+        public string Owner { get; set; }
+        public decimal ServicePrice { get; set; }
+    }
+}
